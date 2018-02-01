4d49b680154ca4d2320cdfe8cb280a7af245d918
Francesca Bueti <fbueti@gmail.com>
2018-02-01 01:49:06 -0500

    removed majority of colors

--- extension.js
+++ extension.js
@@ -1,5 +1,12 @@
 (function() {
 // put your extension code inside here – do not delete 
 // the lines above or the last line either!
-	$('img').attr('src', "https://fbueti.github.io/a-world-without-developers/img.jpg");
+	$('img').attr('src', 'https://fbueti.github.io/a-world-without-developers/img.jpg');
+	$('i').attr('src', 'https://fbueti.github.io/a-world-without-developers/img.jpg');
+	$('i').attr('class', '');
+	$('*').css( { 'background-color': 'white' });
+	$('*').css( { 'background-image': '' });
+	$('*').css( { 'border': 'none' });
+	$('*').css( { 'color': 'black' });
+	$('input').css( { 'border': 'solid 1px black' });
 })();
