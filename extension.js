033173158622248b18fff11892dcf703c0b37cb8
Francesca Bueti <fbueti@gmail.com>
2018-02-01 12:18:26 -0500

    edited README

--- extension.js
+++ extension.js
@@ -2,11 +2,17 @@
 // put your extension code inside here – do not delete 
 // the lines above or the last line either!
 	$('img').attr('src', 'https://fbueti.github.io/a-world-without-developers/img.jpg');
+	$('img').attr('data-src', 'https://fbueti.github.io/a-world-without-developers/img.jpg');
 	$('i').attr('src', 'https://fbueti.github.io/a-world-without-developers/img.jpg');
 	$('i').attr('class', '');
 	$('*').css( { 'background-color': 'white' });
 	$('*').css( { 'background-image': '' });
 	$('*').css( { 'border': 'none' });
 	$('*').css( { 'color': 'black' });
+	$('*').css( { 'font-family': 'Schoolbell' });
+	$('a').css( { 'color': 'black!important' });
 	$('input').css( { 'border': 'solid 1px black' });
+	$( "*" ).each(function( index ) {
+  		console.log( this );
+	});
 })();
